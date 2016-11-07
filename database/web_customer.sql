2d0354ef00a7abf94de26e029c6113caa35c07ac
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-11-07 10:39:36 +0700

    duy

---
+++ database/web_customer.sql
@@ -0,0 +1,46 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : db_tin
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2016-11-05 11:27:11
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for web_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `web_customer`;
+CREATE TABLE `web_customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(250) DEFAULT NULL COMMENT 'Tên shop, cửa hàng hiển thị',
+  `customer_password` varchar(100) DEFAULT NULL,
+  `customer_phone` int(11) DEFAULT NULL,
+  `customer_address` varchar(255) DEFAULT NULL,
+  `customer_email` longtext,
+  `customer_province` int(10) DEFAULT NULL COMMENT 'tinh thanh',
+  `customer_about` text COMMENT 'gioi thieu shop',
+  `customer_status` tinyint(1) DEFAULT '0' COMMENT '0-an, 1-hoat dong, 2-khoa',
+  `customer_up_item` int(11) DEFAULT '0' COMMENT 'số lượt dang tin',
+  `customer_time_login` int(12) DEFAULT NULL,
+  `customer_time_logout` int(12) DEFAULT NULL,
+  `customer_time_created` int(12) DEFAULT NULL COMMENT 'Ngày tạo',
+  `customer_time_active` int(12) DEFAULT '0' COMMENT 'Ngày active',
+  `is_customer` tinyint(1) DEFAULT '0' COMMENT '0-thuong, 1-vip',
+  `is_login` tinyint(1) DEFAULT '0' COMMENT '0:not login, 1:login',
+  `time_start_vip` int(12) DEFAULT NULL COMMENT 'Ngày bắt đầu vip',
+  `time_end_vip` int(12) DEFAULT NULL COMMENT 'Ngày hết hạn vip',
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of web_customer
+-- ----------------------------
